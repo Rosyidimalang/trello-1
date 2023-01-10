a6df4291606746b7956f14c36d0b4e3b4f1dd803
Rosyidimalang <ahmadrosyidi432@gmail.com>
2023-01-10 09:51:01 +0700

    after install react beautiful dnd

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import "./index.css";
+import Input from "./components/Input";
+import Boards from "./components/Boards";
+import { TaskProvider } from "./hooks/useTasks";
 
 function App() {
   return (
-    <div className="layout p-8">
-      <div className="max-w-[26rem] mx-auto">
-        <h1 className="text-blue-500">Hello world!</h1>
-        <p>Hello world!</p>
-        <div className="flex gap-1">
-          <div className="w-4 h-4 rounded-lg bg-bg border border-primary"></div>
-          <div className="w-4 h-4 rounded-lg bg-primary"></div>
-          <div className="w-4 h-4 rounded-lg bg-bg border border-primary"></div>
-          <div className="w-4 h-4 rounded-lg bg-done"></div>
-          <div className="w-4 h-4 rounded-lg bg-done-secondary"></div>
-          <div className="w-4 h-4 rounded-lg bg-ongoing"></div>
-          <div className="w-4 h-4 rounded-lg bg-ongoing-secondary"></div>
-          <div className="w-4 h-4 rounded-lg bg-undone"></div>
-          <div className="w-4 h-4 rounded-lg bg-undone-secondary"></div>
-          <div className="w-4 h-4 rounded-lg bg-board"></div>
-          <div className="w-4 h-4 rounded-lg bg-delete"></div>
+    <div className="overflow-y-scroll h-screen">
+      <div className="layout pt-8">
+        <div className="max-w-[26rem] mx-auto">
+          <TaskProvider>
+            <Input />
+            <Boards />
+          </TaskProvider>
         </div>
       </div>
     </div>
