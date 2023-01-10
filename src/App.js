89e7b816249c84eee78b2aa7d6fe503509cb6c1b
Rosyidimalang <ahmadrosyidi432@gmail.com>
2023-01-10 09:31:21 +0700

    add files and delete files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import "./App.css";
+import "./index.css";
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline text-blue-400">
-        Hello world!
-      </h1>
+    <div className="layout p-8">
+      <div className="max-w-[26rem] mx-auto">
+        <h1 className="text-blue-500">Hello world!</h1>
+        <p>Hello world!</p>
+        <div className="flex gap-1">
+          <div className="w-4 h-4 rounded-lg bg-bg border border-primary"></div>
+          <div className="w-4 h-4 rounded-lg bg-primary"></div>
+          <div className="w-4 h-4 rounded-lg bg-bg border border-primary"></div>
+          <div className="w-4 h-4 rounded-lg bg-done"></div>
+          <div className="w-4 h-4 rounded-lg bg-done-secondary"></div>
+          <div className="w-4 h-4 rounded-lg bg-ongoing"></div>
+          <div className="w-4 h-4 rounded-lg bg-ongoing-secondary"></div>
+          <div className="w-4 h-4 rounded-lg bg-undone"></div>
+          <div className="w-4 h-4 rounded-lg bg-undone-secondary"></div>
+          <div className="w-4 h-4 rounded-lg bg-board"></div>
+          <div className="w-4 h-4 rounded-lg bg-delete"></div>
+        </div>
+      </div>
     </div>
   );
 }
